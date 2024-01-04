6b1c28b6fa0bd6edb45868218b3757cd31a720b0
Aftab1112 <you@example.com>
2024-01-04 09:36:59 +0530

    added

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector(".wrapper")
+const question = document.querySelector(".question")
+const yesBtn = document.querySelector(".yes-btn")
+const noBtn = document.querySelector(".no-btn")
+
+document.querySelector(".no-btn").style.display = "block"
+
+
+const wrapperRect = wrapper.getBoundingClientRect()
+const noBtnRect = noBtn.getBoundingClientRect()
+
+yesBtn.addEventListener("click",() =>{
+    question.innerHTML = "I Love You Too🩷"
+    document.querySelector(".no-btn").style.display = "none"
+
+})
+
+noBtn.addEventListener("click",() =>{
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1
+
+    noBtn.style.left = i + "px"
+    noBtn.style.top = j + "px"
+})
